5972f7759aa1462405814f25c1a61e8a4b228355
Yuri018 <plktmn@gmail.com>
2023-07-24 16:28:32 +0200

    added lesson 5 homework

--- Lesson05/Code/our_boolean/Main.java
+++ Lesson05/Code/our_boolean/Main.java
@@ -1,50 +1,78 @@
 package our_boolean;
 
 public class Main {
-     /*
-     Тип boolean
-     занимает 1 бит и принимает два заначения try and false
-      */
-     public static void main(String[] args) {
-         boolean b;
-         b = false;
-         System.out.println(b);
-         System.out.println(10 < 9);
-         //результатом сравнения в Java яваляется true и false
+    /*
+    Тип boolean
+    занимает 1 бит и принимает два заначения try and false
+     */
+    public static void main(String[] args) {
+        boolean b;
+        b = false;
+        System.out.println(b);
+        System.out.println(10 < 9);
+        //результатом сравнения в Java яваляется true и false
 
-         //Relational operators
-         //оператиоры сравнения
+        //Relational operators
+        //оператиоры сравнения
+         /*
+        >  больше чем
+        <   меньше чем
+        >=  больше или равно
+        <=    меньше или равно
+        ==   равно
+        !=  не равно
 
-         int i = 10;
-         int j = 11;
-         System.out.println("i < j " + (i < j));
-         System.out.println("i > j " + (i > j));
-         System.out.println("i <= j " + (i <= j));
-         System.out.println("i >= j " + (i >= j));
-         System.out.println("i == j " + (i == j));
-         System.out.println("i != j " + (i != j));
+        Результатом выполнения операторов сравнения  будет true  или false.
+        Сравнивать таким образом можно типы , где определен порядок сравнения ( 2>1). Сравнивать переменные типа
+        boolean  не получится, так как не определено что больше - true  или false
+         */
 
-         //Logical operators
-         //Логическме операторы
+        int i = 10;
+        int j = 11;
+        System.out.println("i < j " + (i < j));
+        System.out.println("i > j " + (i > j));
+        System.out.println("i <= j " + (i <= j));
+        System.out.println("i >= j " + (i >= j));
+        System.out.println("i == j " + (i == j));
+        System.out.println("i != j " + (i != j));
+
+        //Logical operators
+        //Логическме операторы
          /*
          & - AND И
          | - OR ИЛИ
          ! - NOT НЕ
          ^ - XOR исключающее ИЛИ
 
-           p         q         p & q        p | q       p^q        !p
-         false     false       false        false
+          p        q         p & q       p | q        p^q        !p
 
+        False   False      False       False        False      True
+        True    False      False       True         True       False
+        False   True       False       True         True       True
+        True    True       True        True         False      False
+        Когда значения p  и q разные , XOR  дает true.  Когда одинаковые, то false
           */
 
-         boolean b1 = true;
-         boolean b2 = false;
-         System.out.println(b1 & b2); //false
-         System.out.println(!(b1 & b2)); //true
-         System.out.println(b1 | b2);  //true
-         System.out.println(b1 ^ b2); //true
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println(b1 & b2); //false
+        System.out.println(!(b1 & b2)); //true
+        System.out.println(b1 | b2);  //true
+        System.out.println(b1 ^ b2); //true
 
+        //&& -  Conditional-AND  and
+        //        || -  Conditional-OR
+        //  эти операторы будут проверять второй операнд только в случае необходимости, если результат не понятен.
+        // Обычные OR and AND  будут всегда проверять оба операнда.
 
+        int n, d;
 
-     }
+        n = 10;
+        d = 0;
+
+        if (d != 0 && (n % d) == 0) {
+            System.out.println("n can be divided by d without remainder");
+        }
+
+    }
 }
