d865aa286d1319d5896ff52261a7ba3b9b05338a
Yuri018 <plktmn@gmail.com>
2023-08-07 13:19:46 +0200

    added Lesson 15 solution optimization

--- Lesson15/code/practicum/Main15.java
+++ Lesson15/code/practicum/Main15.java
@@ -2,11 +2,12 @@ package practicum;
 
 public class Main15 {
     public static void main(String[] args) {
-        //найти последнее минусовое число в массиве и вывести на экран. Если таких нет вывести 0.
+        //find the last negative number in the array and display it on the screen. If there are none, output 0.
 
         int[] arrayNum = {14, 73, 28, 8, 15, 45, 0, -21, 999};
 
         System.out.println(findLastNegativeNumber(arrayNum));
+        System.out.println(findLastNegativeNumber2(arrayNum));
     }
 
     public static int findLastNegativeNumber(int[] arr) {
@@ -14,6 +15,16 @@ public class Main15 {
         for (int j : arr) {
             if (j < 0) {
                 lastNum = j;
+            }
+        }
+        return lastNum;
+    }
+
+    public static int findLastNegativeNumber2(int[] arr){
+        int lastNum = 0;
+        for (int i = arr.length - 1; i >= 0; i--){//traverse the array from the end
+            if (arr[i] < 0){
+                return arr[i];
             }
         }
         return lastNum;
