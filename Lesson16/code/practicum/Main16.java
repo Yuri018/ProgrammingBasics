d8f228bee2f673d4cc5cb180ebcc5521ca4989ac
Yuri018 <plktmn@gmail.com>
2023-08-08 10:01:31 +0200

    GIT test 2

--- Lesson16/code/practicum/Main16.java
+++ Lesson16/code/practicum/Main16.java
@@ -5,13 +5,19 @@ public class Main16 {
         //Написать метод,, который принимает 3 числа int (day, month, year) возвращает строку вида "08. August 2023 "
 
         int day = 8;
-        String month ="August";
+        int month = 8;
         int year = 2023;
 
         showDayOfThYear(day, month, year);
     }
 
-    public static void showDayOfThYear(int d, String m, int year){
-        System.out.println("0" + d + ". " + m + " " + year);
+    public static void showDayOfThYear(int d, int m, int year) {
+        switch (m){
+            case 8 : {
+                String month = "August";
+                System.out.println("0" + d + ". " + month + " " + year);
+                break;
+            }
+        }
     }
 }
