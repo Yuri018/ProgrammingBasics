59a17a0a7c2712c2515786f041bbfc3df4d3c558
Yuri018 <plktmn@gmail.com>
2023-08-28 18:30:49 +0200

    Implemented a project on UML class diagram "garage". Lesson 29

---
+++ Lesson29/src/ait/cars/dao/GarageImpl.java
@@ -0,0 +1,48 @@
+package ait.cars.dao;
+
+import ait.cars.model.Car;
+
+public class GarageImpl implements Garage{
+    private Car[] cars;
+    private int size;
+
+    public GarageImpl(int capacity) {
+        cars = new Car[capacity];
+    }
+
+    @Override
+    public boolean addCar(Car car) {
+        return false;
+    }
+
+    @Override
+    public Car removeCar(String regNumber) {
+        return null;
+    }
+
+    @Override
+    public Car findCarByRegNumber(String regNumber) {
+        return null;
+    }
+
+    @Override
+    public Car[] findCarsByModel(String model) {
+        return new Car[0];
+    }
+
+    @Override
+    public Car[] findCarsByCompany(String company) {
+        return new Car[0];
+    }
+
+    @Override
+    public Car[] findCarsByEngine(double min, double max) {
+        return new Car[0];
+    }
+
+    @Override
+    public Car[] findCarsByColor(String color) {
+        return new Car[0];
+    }
+
+}
---
+++ Lesson29/src/ait/cars/test/GarageTest.java
@@ -0,0 +1,42 @@
+package ait.cars.test;
+
+import ait.cars.dao.Garage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GarageTest {
+
+    private Garage garage;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addCar() {
+    }
+
+    @Test
+    void removeCar() {
+    }
+
+    @Test
+    void findCarByRegNumber() {
+    }
+
+    @Test
+    void findCarsByModel() {
+    }
+
+    @Test
+    void findCarsByCompany() {
+    }
+
+    @Test
+    void findCarsByEngine() {
+    }
+
+    @Test
+    void findCarsByColor() {
+    }
+}
